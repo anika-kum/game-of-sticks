950bac7da02fb24453b18533bea2938dc445d1b8
anikakumar <anikakumar@192.168.7.197>
2023-11-22 10:29:45 -0800

    Non tested trained AI

--- game-of-sticks-1/src/main/GameOfSticks.java
+++ game-of-sticks-1/src/main/GameOfSticks.java
@@ -3,12 +3,15 @@ import java.util.*;
 public class GameOfSticks {
 	
 	private static ArrayList<ArrayList<Integer>> computerHats = new ArrayList<ArrayList<Integer>>(); //each hat has balls in it numbered 1-3
-	private static ArrayList<IndexBall> AIGoodResponses = new ArrayList<IndexBall>();
+	private static ArrayList<IndexBall> AIResponses = new ArrayList<IndexBall>();
+	private static ArrayList<ArrayList<Integer>> trainedComputerHats1 = new ArrayList<ArrayList<Integer>>(); //for trained model
+	private static ArrayList<IndexBall> AIResponses1 = new ArrayList<IndexBall>(); //for trained model
+	private static ArrayList<ArrayList<Integer>> trainedComputerHats2 = new ArrayList<ArrayList<Integer>>(); //for trained model
+	private static ArrayList<IndexBall> AIResponses2 = new ArrayList<IndexBall>(); //for trained model
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Game of Sticks! \n");
 		options(new Scanner(System.in));
-		//AIVSPlayer();
 	}
 	
 	static void playerVSPlayer() {
@@ -57,24 +60,72 @@ public class GameOfSticks {
 			else {
 				int compNumIndices = computerHats.get(numSticks-1).size();
 				int AIResponse = computerHats.get(numSticks-1).get((int) (Math.random()*compNumIndices));
-				AIGoodResponses.add(new IndexBall(numSticks, AIResponse));
+				AIResponses.add(new IndexBall(numSticks, AIResponse));
 				System.out.println("AI selects " + AIResponse);
 				numSticks-=AIResponse;
 			}
 			counter++;
 		}
 		if ((((counter + 1) % 2) + 1) == 1) { //if AI wins
-			IndexBall randGoodResponse1 = AIGoodResponses.get((int) (Math.random()*AIGoodResponses.size()));
-			IndexBall randGoodResponse2 = AIGoodResponses.get((int) (Math.random()*AIGoodResponses.size()));
+			IndexBall randGoodResponse1 = AIResponses.get((int) (Math.random()*AIResponses.size()));
+			IndexBall randGoodResponse2 = AIResponses.get((int) (Math.random()*AIResponses.size()));
 			computerHats.get(randGoodResponse1.getIndex()).add(randGoodResponse1.getBall());
 			computerHats.get(randGoodResponse2.getIndex()).add(randGoodResponse2.getBall());
 			System.out.println("You lose.");
 			playAgain(scan);
 		}
 		else {
-			AIGoodResponses = new ArrayList<IndexBall>(); 
+			AIResponses = new ArrayList<IndexBall>(); 
 			System.out.println("AI loses.");
 			playAgain(scan);
+		}
+	}
+	
+	static void AIVSAI() {
+		int numSticks = (int) (Math.random()*90+10);
+
+		for (int i=0; i<numSticks; i++) { //filling in initial outer level ArrayList
+			trainedComputerHats1.add(new ArrayList<Integer>());
+			trainedComputerHats2.add(new ArrayList<Integer>());
+		}
+		
+		for (int i=0; i<numSticks; i++) { //populating initial balls in inner ArrayList
+			trainedComputerHats1.get(i).add(1);
+			trainedComputerHats1.get(i).add(2);
+			trainedComputerHats1.get(i).add(3);
+			
+			trainedComputerHats2.get(i).add(1);
+			trainedComputerHats2.get(i).add(2);
+			trainedComputerHats2.get(i).add(3);
+		}
+		
+		int counter = 0;
+		while (numSticks > 0) {
+			if (counter % 2 == 0) {
+				int compNumIndices1 = computerHats.get(numSticks-1).size();
+				int AIResponse1 = computerHats.get(numSticks-1).get((int) (Math.random()*compNumIndices1));
+				AIResponses1.add(new IndexBall(numSticks, AIResponse1));
+				numSticks-=AIResponse1;
+			}
+			else {
+				int compNumIndices2 = computerHats.get(numSticks-1).size();
+				int AIResponse2 = computerHats.get(numSticks-1).get((int) (Math.random()*compNumIndices2));
+				AIResponses2.add(new IndexBall(numSticks, AIResponse2));
+				numSticks-=AIResponse2;
+			}
+			counter++;
+		}
+		if ((((counter + 1) % 2) + 1) == 1) { //if AI2 wins
+			IndexBall randGoodResponse1Second = AIResponses2.get((int) (Math.random()*AIResponses2.size()));
+			IndexBall randGoodResponse2Second = AIResponses.get((int) (Math.random()*AIResponses2.size()));
+			trainedComputerHats1.get(randGoodResponse1Second.getIndex()).add(randGoodResponse1Second.getBall());
+			trainedComputerHats1.get(randGoodResponse2Second.getIndex()).add(randGoodResponse2Second.getBall());
+		}
+		else { //if AI1 wins
+			IndexBall randGoodResponse1First = AIResponses1.get((int) (Math.random()*AIResponses1.size()));
+			IndexBall randGoodResponse2First = AIResponses1.get((int) (Math.random()*AIResponses1.size()));
+			trainedComputerHats1.get(randGoodResponse1First.getIndex()).add(randGoodResponse1First.getBall());
+			trainedComputerHats1.get(randGoodResponse2First.getIndex()).add(randGoodResponse2First.getBall());
 		}
 	}
 	
